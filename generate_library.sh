d9b37b4b7c7cd3da24df73fd0d071d1c570e134d
Maurycy Frydecki <maurycy.frydecki@gmail.com>
2025-08-08 19:04:46 +0200

    initial commit

---
+++ generate_library.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+OUT_DIR="WsCK_Footprint_Library.pretty"
+SUBMODULES_DIR="submodules"
+
+echo "[*] Initializing & updating submodules..."
+for dir in submodules/*; do
+    if [ -d "$dir" ]; then
+        name=$(basename "$dir")
+        git config -f .gitmodules submodule.submodules/$name.branch main
+    fi
+done
+git submodule sync
+git submodule update --init --remote --checkout --recursive
+
+echo "[*] Cleaning output directory..."
+mkdir -p "$OUT_DIR"
+rm -rf "$OUT_DIR"/*
+
+echo "[*] Extracting .kicad_mod files..."
+for mod in "$SUBMODULES_DIR"/*/; do
+    echo "[*]   Processing submodule: $mod"
+    for file in "$mod"*.kicad_mod; do
+        if [ -f "$file" ]; then
+            echo "[*]     Copying $file"
+            cp "$file" "$OUT_DIR"
+        fi
+    done
+done
+
+echo "[*] Cleaning $SUBMODULES_DIR directory..."
+for dir in "$SUBMODULES_DIR"/*; do
+    if [ -d "$dir" ]; then
+        rm -rf "$dir"/.[!.]*
+    fi
+done
+
+
+echo "[✓] Library generated in $OUT_DIR/"
